e7f50e314c91a4c9f3fa1a89596213cbd5bec743
unknown <nasreddinejaouadi@hotmail.com>
2021-11-17 01:32:46 +0100

    some changes

--- text_editor.py
+++ text_editor.py
@@ -1,5 +1,16 @@
 from tkinter import *  
 #creating the application main window.   
-top = Tk()  
-#Entering the event main loop  
+top = Tk()
+#get the screen size
+screen_width = top.winfo_screenwidth()
+screen_height = top.winfo_screenheight()
+#Entering the event main loop
+#the frame geometrie
+top.geometry(f"{screen_width}x{screen_height}")
+menubutton = Menubutton(top, text = "file", relief = FLAT)
+menubutton.grid()  
+  
+menubutton.menu = Menu(menubutton)
+menubutton["menu"]=menubutton.menu  
+
 top.mainloop()  
